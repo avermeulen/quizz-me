8e26ca5c05d2761de8eee18ed27d9ad8e27f5bb9
avermeulen <andre.vermeulen@gmail.com>
2016-09-23 22:34:41 +0200

    quiz allocation function decoupled

--- routes/quiz-routes.js
+++ routes/quiz-routes.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose'),
     ObjectId = mongoose.Types.ObjectId,
-    render = require('../utilities/render'),
     _ = require('lodash'),
+    render = require('../utilities/render'),
+    AllocateQuiz = require('../utilities/allocate-quiz'),
     quizResultsBuilder = require('../utilities/quiz-results-builder');
 
 module.exports = function(models) {
 
     var Course = models.Course,
         Quiz = models.Questionairre,
-        User = models.User;
+        User = models.User,
+        allocateQuiz = AllocateQuiz(models);
 
     function findQuizById(quiz_id) {
         return models.Questionairre
@@ -166,42 +168,7 @@ module.exports = function(models) {
         .catch((err) => next(err));
     };
 
-    function allocateQuiz(course_id, user_id, question_count){
 
-        var course_id = course_id instanceof ObjectId ? course_id :  ObjectId(course_id),
-            user_id = user_id instanceof ObjectId ? user_id : ObjectId(user_id),
-            question_count = question_count || 3;
-
-        return Quiz.find({
-                _course: course_id,
-                _user: user_id,
-                status : 'active'
-            })
-            .then((quizzes) => {
-                if (quizzes.length === 0) {
-
-                    return Course.findById(course_id,
-                            '-_id -questions._id -questions.options._id -__v')
-                        .then((course) => {
-
-                            var questions = _.sampleSize(course.questions, question_count);
-                            var shuffleOptions = (question) => question.options = _.shuffle(question.options);
-                            questions.forEach(shuffleOptions);
-                            delete course.questions;
-                            course.questions = questions;
-
-                            var quiz = Quiz({
-                                _user: user_id,
-                                _course : course_id,
-                                details: course
-                            });
-                            return quiz.save();
-                        });
-                } else {
-                    return {user_id : user_id, status : 'already allocated'};
-                }
-            });
-    };
 
     var allocateQuizToUsers = function(req, res){
 
@@ -218,7 +185,7 @@ module.exports = function(models) {
 
         candidateIds = Array.isArray(candidateIds) ? candidateIds : [candidateIds];
         var allocations = candidateIds.map((candidate_id) => {
-            return allocateQuiz(course_id, candidate_id, 3);
+            return allocateQuiz.allocate(course_id, candidate_id, 3);
         });
 
         Promise
---
+++ utilities/allocate-quiz.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose'), 
+    _ = require('lodash'),
+    ObjectId = mongoose.Types.ObjectId;
+
+module.exports = function(models){
+
+    var Course = models.Course,
+        Quiz = models.Questionairre,
+        User = models.User;
+
+    function allocateQuiz(course_id, user_id, question_count){
+
+        var course_id = course_id instanceof ObjectId ? course_id :  ObjectId(course_id),
+            user_id = user_id instanceof ObjectId ? user_id : ObjectId(user_id),
+            question_count = question_count || 3;
+
+        return Quiz.find({
+                _course: course_id,
+                _user: user_id,
+                status : 'active'
+            })
+            .then((quizzes) => {
+                if (quizzes.length === 0) {
+
+                    return Course.findById(course_id,
+                            '-_id -questions._id -questions.options._id -__v')
+                        .then((course) => {
+
+                            var questions = _.sampleSize(course.questions, question_count);
+                            var shuffleOptions = (question) => question.options = _.shuffle(question.options);
+                            questions.forEach(shuffleOptions);
+                            delete course.questions;
+                            course.questions = questions;
+
+                            var quiz = Quiz({
+                                _user: user_id,
+                                _course : course_id,
+                                details: course
+                            });
+                            return quiz.save();
+                        });
+                } else {
+                    return {user_id : user_id, status : 'already allocated'};
+                }
+            });
+    };
+
+    return {
+        allocate : allocateQuiz
+    }
+}
