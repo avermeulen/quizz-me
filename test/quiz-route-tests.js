8cf59b0b1b4b59141c0d9fabe93b5487fa59966c
avermeulen <andre.vermeulen@gmail.com>
2016-10-15 17:22:59 +0200

    added test for quiz show & answering last question

--- test/quiz-route-tests.js
+++ test/quiz-route-tests.js
@@ -1,6 +1,7 @@
 
 const assert = require('assert'),
     quizAnswerData = require('./quiz-answer-data'),
+    quizLastAnswerData = require('./quiz-result-answer-last-question'),
     Promise = require('bluebird'),
     co = require('co'),
     mongoose = require('mongoose'),
@@ -11,61 +12,150 @@ const assert = require('assert'),
 
 require('co-mocha');
 
+var requestCreator = function(params){
+    return {
+        path : "",
+        params : {
+            quiz_id : params.quiz_id,
+            question_nr : params.question_nr
+        },
+        body : {
+            answer_id : params.answer_id,
+            questionType : params.questionType
+        },
+        checkBody : function(){
+            return {
+                notEmpty : function(){
+                }
+            }
+        }
+    };
+}
+
 describe('Quiz routes', function(){
     mongooseConnect();
 
     const QUIZ_ID = '57c6da441fb2c0ac907f6746';
 
-    beforeEach(function*(){
-            yield models.Questionairre.remove({});
+    describe('answer one question ', function(){
 
-            //var quizzes = yield models.Questionairre.find({});
-            //console.log(quizzes);
+        beforeEach(function*(){
+                yield models.Questionairre.remove({});
+                var quiz = new models
+                    .Questionairre(quizAnswerData);
+                yield quiz.save();
+        });
+
+        it("should show question", function(done){
+            var quizRoute = new QuizRoutes(models);
+
+            var res = {
+                render : function(view, context){
+
+                    try{
+                        var expectedContext = {
+                            name: 'Number game',
+                            quiz_id: '57c6da441fb2c0ac907f6746',
+                            progress_message: 'Question 1 of 3',
+                            questionType: 'mcq'
+                        }
+                        console.log('...');
+
+                        assert.equal(context.quiz_id, expectedContext.quiz_id);
+                        assert.equal(context.questionType, expectedContext.questionType);
+                        assert.equal(context.name, expectedContext.name);
+                        assert.equal(context.progress_message,
+                                expectedContext.progress_message);
 
 
-            var quiz = new models
-                .Questionairre(quizAnswerData);
-            yield quiz.save();
-
-
-    });
-
-    it("should support answering a question", function(done){
-        var quizRoute = new QuizRoutes(models);
-
-        var res = {
-            redirect : function(to){
-                assert.equal(to, '/quiz/57c6da441fb2c0ac907f6746/answer/1');
-
-                models.Questionairre
-                    .findById(QUIZ_ID).then((quiz) => {
-                        assert.equal(quiz.answers.length, 1);
                         done();
-                    });
-            }
-        };
-
-        var req = {
-            params : {
-                quiz_id : QUIZ_ID,
-                question_nr : 0
-            },
-            body : {
-
-            },
-            checkBody : function(){
-                return {
-                    notEmpty : function(){
-
+                    }
+                    catch(err){
+                        done(err);
                     }
                 }
-            }
-        };
+            };
 
-        quizRoute.answerQuizQuestion(req, res, function(err){
-            done(err);
+            var req = requestCreator({
+                quiz_id : QUIZ_ID,
+                questionType : 'mcq',
+                question_nr : 0,
+                answer_id : '57c6da441fb2c0ac907f6753'
+            });
+
+            quizRoute.showQuizQuestion(req, res, function(err){
+                 done(err);
+            });
+        });
+
+        it("should support answering a question", function(done){
+            var quizRoute = new QuizRoutes(models);
+
+            var res = {
+                redirect : function(to){
+                    assert.equal(to, '/quiz/57c6da441fb2c0ac907f6746/answer/1');
+
+                    models.Questionairre
+                        .findById(QUIZ_ID).then((quiz) => {
+                            assert.equal(quiz.answers.length, 1);
+                            assert.equal(quiz.answers[0]._answer, '57c6da441fb2c0ac907f6753');
+                            done();
+                        });
+                }
+            };
+
+            var req = requestCreator({
+                quiz_id : QUIZ_ID,
+                questionType : 'mcq',
+                question_nr : 0,
+                answer_id : '57c6da441fb2c0ac907f6753'
+            });
+
+            quizRoute.answerQuizQuestion(req, res, function(err){
+                done(err);
+            });
         });
 
     });
 
+    describe('answering last question', function(){
+
+        before(function*(){
+                yield models.Questionairre.remove({});
+                var quiz = new models
+                    .Questionairre(quizLastAnswerData);
+                yield quiz.save();
+        });
+
+        it("should succeed", function(done){
+            var quizRoute = new QuizRoutes(models);
+
+            var res = {
+                redirect : function(to){
+                    assert.equal(to, '/quiz/57c6da441fb2c0ac907f6746/completed');
+
+                    models.Questionairre
+                        .findById(QUIZ_ID).then((quiz) => {
+                            assert.equal(quiz.answers.length, 3);
+                            assert.equal(quiz.answers[2]._answer, '57c6da441fb2c0ac907f6749');
+                            assert.equal(quiz.status, 'completed');
+                            assert.equal(quiz.score, 67);
+                            assert.deepEqual(quiz.answers.map((a) => a.correct), [false, true, true]);
+                            done();
+                        });
+                }
+            };
+
+            var req = requestCreator({
+                quiz_id : QUIZ_ID,
+                questionType : 'mcq',
+                question_nr : 2,
+                answer_id : '57c6da441fb2c0ac907f6749'
+            });
+
+            quizRoute.answerQuizQuestion(req, res, function(err){
+                done(err);
+            });
+        });
+    });
 });
