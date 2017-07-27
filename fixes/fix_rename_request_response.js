3bc5caed44c6925b27f6926dd5f85c7cfa1b866f
avermeulen <andre.vermeulen@gmail.com>
2017-07-27 21:53:00 +0200

    added initial fix

---
+++ fixes/fix_rename_request_response.js
@@ -0,0 +1,33 @@
+const models = require("../models");
+const mongoose = require("mongoose");
+
+var Questionairre = models.Questionairre;
+
+async function connect(){
+    var mongoDatabaseUrl = process.env.MONGODB_URL || 'mongodb://localhost/quizz_me';
+    await mongoose.connect(mongoDatabaseUrl, {}).connection;
+}
+
+async function findQuestion(questionText){
+    var quizzes = await Questionairre.find({
+        "details.questions.question" : questionText
+    });
+
+    for (var quiz of quizzes){
+        
+        let question = await quiz.details.questions.findOne( { question : questionText } );
+        console.log(question);
+
+    }
+
+}
+
+try{
+    connect();
+    findQuestion("What does the HttpRequest render function do?");
+    console.log("done!");
+    mongoose.connection.close();
+}
+catch(e){
+    console.log(e);
+}
