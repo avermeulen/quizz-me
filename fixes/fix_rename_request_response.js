b187f15e3ab265f74ae65ed96b9e4fd281060fd2
avermeulen <andre.vermeulen@gmail.com>
2017-07-28 09:26:06 +0200

    fixed quiz typo

--- fixes/fix_rename_request_response.js
+++ fixes/fix_rename_request_response.js
@@ -45,13 +45,18 @@ async function doIt(){
         
         await connect();
 
-        await findQuestion("What does the HttpRequest render function do?", 
-            "What does the HttpResponse render function do?");
+        //await findQuestion("What does the HttpRequest render function do?", 
+        //    "What does the HttpResponse render function do?");
         
         console.log("--------------------------");
 
-        await findQuestion("What is the difference between the HttpRequest send and render methods?", 
-            "What is the difference between the HttpResponse send and render methods?");
+        //await findQuestion("What is the difference between the HttpRequest send and render methods?", 
+        //    "What is the difference between the HttpResponse send and render methods?");
+
+        await findQuestion("Tell us which school did you completed your Senior Certificate at. If you have any other tertiary education, tell us what you studied and where.", 
+            "Tell us which school you completed your Senior Certificate at. If you have any other tertiary education, tell us what you studied and where.");
+        
+        
 
         console.log("done!");
         mongoose.connection.close();
